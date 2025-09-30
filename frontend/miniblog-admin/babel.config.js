43b82e4e9388d389a33a5dde419187684ce506bd
607163943 <q45962219@126.com>
2025-09-30 12:49:05 +0800

    实现了Layout布局

--- frontend/miniblog-admin/babel.config.js
+++ frontend/miniblog-admin/babel.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  // 配置ElementUI按需自动导入
+  plugins: [
+    [
+      'component',
+      {
+        libraryName: 'element-ui',
+        styleLibraryName: 'theme-chalk'
+      }
+    ]
   ]
 }
---
+++ frontend/miniblog-admin/src/views/layout/aside-nav.vue
@@ -0,0 +1,49 @@
+<template>
+  <el-aside width="200px" class="aside-nav">
+    <el-menu default-active="/dashboard" class="el-menu-vertical-demo" text-color="#606266" active-text-color="#409EFF" :router="true">
+      <el-menu-item index="/dashboard">
+        <i class="el-icon-data-board"></i>
+        <span slot="title">工作台</span>
+      </el-menu-item>
+      <el-menu-item index="/article">
+        <i class="el-icon-document"></i>
+        <span slot="title">文章管理</span>
+      </el-menu-item>
+      <el-menu-item index="/column">
+        <i class="el-icon-collection"></i>
+        <span slot="title">专栏管理</span>
+      </el-menu-item>
+      <el-menu-item index="/tag">
+        <i class="el-icon-collection-tag"></i>
+        <span slot="title">标签管理</span>
+      </el-menu-item>
+    </el-menu>
+  </el-aside>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less">
+.el-aside {
+  text-align: center;
+  line-height: 200px;
+}
+
+.aside-nav {
+  .el-menu {
+    border-right: 1px solid #F5F7FA;
+    height: 100%;
+    background-color: #F5F7FA;
+
+    .el-menu-item {
+      &:hover {
+        background-color: #E6F0FF;
+      }
+    }
+  }
+}
+</style>
